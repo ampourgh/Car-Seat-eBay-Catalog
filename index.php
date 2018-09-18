1ccb6a7cece4d99d2f1fd050ba894e12d8a163fb
ampourgh <ampourgh@gmail.com>
2018-09-17 23:06:04 -0600

    bug: adjust animation
    The animation from the footer was having issues, with the navigation menu being inside the footer tag, and the site-outer closing tag not being around the footer or the navigation.

--- index.php
+++ index.php
@@ -386,33 +386,7 @@
           <a href="#"><i class="fa fa-amazon" style="height: 42px; width: auto; padding-top: 10px;"></i></a>
 
         </div>
-
-        <!-- NAVIGATION -->
-        <div class="navigation">
-          <div class="navigation-content">
-        	<h3 class="blue">Menu</h3>
-        	<ul class="navigation-menu">
-        	  <li class="blue"><a href="./index.php" class="active">Home</a></li>
-        	  <li><b>CARS:</b></li>
-        	  <li><a href="./index.php#chevrolet"><i class="blue fa fa-circle"></i> Chevrolet</a></li>
-        	  <li><a href="./index.php#dodge"><i class="blue fa fa-circle"></i> Dodge</a></li>
-        	  <li><a href="./index.php#ford"><i class="blue fa fa-circle"></i> Ford</a></li>
-        	  <li><a href="./index.php#gmc"><i class="blue fa fa-circle"></i> GMC</a></li>
-        	  <li><a href="./index.php#hummer"><i class="blue fa fa-circle"></i> Hummer</a></li>
-        	  <li><a href="./index.php#jeep"><i class="blue fa fa-circle"></i> Jeep</a></li>
-        	  <li><a href="./index.php#lincoln"><i class="blue fa fa-circle"></i> Lincoln</a></li>
-        	</ul>
-        	<h3 class="blue">More</h3>
-        	<ul class="navigation-menu">
-        	  <li><a href="./navbar/about.php">About Us</a></li>
-        	  <li><a href="./navbar/contact.php">Contact</a></li>
-        	  <li><a href="./navbar/services.php">Services</a></li>
-        	  <li><a href="./navbar/FAQ.php">FAQ</a></li>
-        	</ul>
-          </div>
-        </div>
-
-
+        
       </div>
 
     </footer>
